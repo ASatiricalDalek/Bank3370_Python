51b8d37e5f3763c373eb6b4b353c9b999a21ef33
connor <dalek124@gmail.com>
2019-03-28 15:34:54 -0400

    Basic database code and tests

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask, render_template, request
+from dbconnector import simpleQuery, get_db
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def hello_world():
-    return render_template('home.html')
+    patrons = simpleQuery("patron", "patronFirstName, patronLastName")
+    if request.method == "POST":
+        db = get_db()
+        cur = db.cursor()
 
+        selectedPatron = request.form['listOfPatrons']
+
+        cur.execute("SELECT patronFirstName, patronLastName, patronEmail FROM patron WHERE patronLastName = " + "'" + selectedPatron + "'")
+        patronInfo = cur.fetchall()
+
+        return render_template('accounts.html', html_patrons = patrons, firstName = patronInfo[0])
+
+    else:
+        return render_template('accounts.html', html_patrons=patrons)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- dbconnector.py
+++ dbconnector.py
@@ -1,4 +1,19 @@
 import sqlite3
 from flask import g
 
-DATABASE = '/bank.db'
+# Path is NOT relative
+DATABASE = '/home/connor/Documents/softwareEng/bank3370_PythonGit/bank.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+
+def simpleQuery(table, row):
+    cur = get_db().cursor()
+    cur.execute("SELECT Id " + row + " FROM " + table)
+    result = cur.fetchall()
+    return result
+
